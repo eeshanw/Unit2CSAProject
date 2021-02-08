799729fedd33f5bdc5b0fab2771c56f122bd59b3
eeshanw <eeshanwalia@gmail.com>
2021-02-07 20:38:34 -0800

    final commit

--- NFLStatsCode.py
+++ NFLStatsCode.py
@@ -26,35 +26,54 @@ def referenceSingleYear(csv, y):
 
 #finds entries for a certain position in a certain year
 def referenceSingleYearPos(csv, y, p):
-    print(csv.loc[(csv.YEAR == y) & (csv.Positions == 'QB')])
+    print(csv.loc[(csv.YEAR == y) & (csv.Positions == p)])
 
+#averages the stats of all players from a certain position
 def avgPerPos(csv, p):
     avgPlayerChart = csv.loc[df.Positions == p]
     averageStats = avgPlayerChart.mean(axis=0)
     print(averageStats)
 
+#searches for all occurances with a certain team
+def teamStat(csv, t):
+    csv = csv.loc[csv.TEAM == t]
+    print(csv)
+
+#returns all players with a minimum of v yards thrown
 def minStatisticYDS(csv, v):
     csv = csv.loc[csv.YDS >= v]
     print(csv)
 
+#returns all players with a minimum of v touchdowns thrown
 def minStatisticTD(csv, v):
     csv = csv.loc[csv.TD >= v]
     print(csv)
 
+#returns all players with a minimum of v pass attempts
 def minStatisticATT(csv, v):
     csv = csv.loc[csv.ATT >= v]
     print(csv)
 
+#returns all players with v completions or more
 def minStatisticCOMP(csv, v):
     csv = csv.loc[csv.COMP >= v]
     print(csv)
 
+#returns the average of all the entries in the dataframe
 def totalAverage(csv):
     averageChart = csv.drop('YEAR', axis=1)
     averageStats = averageChart.mean(axis=0)
     print('Average stats:')
     print(averageStats)
 
+#returns the average of a single players
+def playerAverage(csv, p):
+    playerchart = (csv.loc[csv.PLAYER == q2])
+    playerchart = playerchart.drop('YEAR', axis=1)
+    average = playerchart.mean(axis=0)
+    print(average)
+
+#plots the spread of all players who threw a pass from 2007 to 2016
 def plotPositions(csv):
     freqdict = {}
     totallist = []
@@ -109,29 +128,29 @@ if (question == "Single"):
         referenceSingle(df, name)
     elif (q1 == "Year"):
         year = float(input("Enter the year: "))
-        pos = str(input("What position would you like to search for (QB or ALL): "))
-        if (pos == "QB"):
-            referenceSingleYearPos(df, year, pos)
-        elif (pos == "ALL"):
+        pos = str(input("What position would you like to search for (Specific position or ALL): "))
+        if (pos == "ALL"):
             referenceSingleYear(df, year)
+        else:
+            referenceSingleYearPos(df, year, pos)
 
 elif (question == "Cumulative"):
     q3 = input("Would you like to search for Player or Position: ")
     if (q3 == "Player"):
-        q2 = input("Enter the name of the player: ")
-        playerchart = (df.loc[df.PLAYER == q2])
-        playerchart = playerchart.drop('YEAR', axis=1)
-        average = playerchart.mean(axis=0)
-        print(average)
+        q2 = str(input("Enter the name of the player: "))
+        playerAverage(df, q2)
     elif (q3 == "Position"):
-        q4 = input("What statistic do you want to search for: Overall (ovr), Average per position (avperpos), Minimum stat (minstat): ")
+        q4 = input("What statistic do you want to search for: Overall (ovr), Average per position (avperpos), Minimum stat (minstat), By team (team): ")
         if (q4 == "ovr"):
             totalAverage(df)
             print('Total number of players '+ str(len(df)))
             qG = input("Would you like to see the position breakdown as a graph (yes/no): ")
             if (qG == "yes"):
                 plotPositions(df)
-        if (q4 == "avperpos"):
+        elif (q4 == "team"):
+            team = str(input("Which team would you like to search for: "))
+            teamStat(csv, team)
+        elif (q4 == "avperpos"):
             q5 = str(input("Which position would you like to search for: "))
             avgPerPos(df, q5)
         elif (q4 == "minstat"):
